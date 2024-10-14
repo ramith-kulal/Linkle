a8e5aa99a3dd11b9af525c187f398a20663d8594
Ramith Kulal <ramithnr1234@gmail.com>
2024-10-14 18:33:23 +0530

    Update Home.tsx

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,6 +1,5 @@
 import Navbar from "../components/Navbar"
 import HeroSection from '../components/HeroSection'
-import React from "react"
 export default function () {
   return (
     <div><Navbar/> 
